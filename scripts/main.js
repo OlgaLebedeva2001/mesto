74895772ef372e5c20b35aa27cff905f49e33d31
Olga Lebedeva <olga2001lebedeva@yandex.ru>
2022-12-17 03:24:22 +0300

    Добавили попап для добавления картинок

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,48 @@
-const openPopupButton = document.querySelector(".info__edit-button");
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+//////////////////////////////////////////////////////////////////
+const editPopupButton = document.querySelector(".info__edit-button");
 const closePopupButton = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
 let infoTitle = document.querySelector(".info__title");
 let infoSubtitle = document.querySelector(".info__subtitle");
-// Находим форму в DOM
-let formElement = document.querySelector(".popup__form"); //  Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__input_field_name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector(".popup__input_field_job"); // Воспользуйтесь инструментом .querySelector()
-
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_field_name");
+let jobInput = document.querySelector(".popup__input_field_job");
+/////////////////////////////////////////////////////////////
+let addPopupButton = document.querySelector(".profile__add-button");
+const closePopup2Button = document.querySelector(".popup2__close");
+const popup2 = document.querySelector(".popup2");
+let titleInput = document.querySelector(".popup2__input_field_title");
+let linkInput = document.querySelector(".popup2__input_field_link");
+let form2Element = document.querySelector(".popup2__form");
+let pictureTitle = document.querySelector(".description__title");
+let pictureLink = document.querySelector(".element__image");
+//////////////////////////////////////////////////////////////
 function openPopup() {
   popup.classList.add("popup_opened");
   nameInput.value = infoTitle.textContent;
@@ -18,9 +52,16 @@ function openPopup() {
 function closePopup() {
   popup.classList.remove("popup_opened");
 }
+/////////////////////////////////////////////////////////////
+function openPopup2() {
+  popup2.classList.add("popup2_opened");
+}
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+function closePopup2() {
+  popup2.classList.remove("popup2_opened");
+}
+///////////////////////////////////////////////////////////////
+// Обработчик «отправки» формы
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Получите значение полей jobInput и nameInput из свойства value
@@ -30,8 +71,21 @@ function handleFormSubmit(evt) {
   infoSubtitle.textContent = jobInput.value;
   closePopup();
 }
-
-openPopupButton.addEventListener("click", openPopup);
+//////////////////////////////////////////////////////////////////
+function addFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Получите значение полей jobInput и nameInput из свойства value
+  // Выберите элементы, куда должны быть вставлены значения полей
+  // Вставьте новые значения с помощью textContent
+  pictureTitle.textContent = titleInput.value;
+  pictureLink.textContent = linkInput.value;
+  closePopup2();
+}
+//////////////////////////////////////////////////////////////////
+editPopupButton.addEventListener("click", openPopup);
 closePopupButton.addEventListener("click", closePopup);
-// Прикрепляем обработчик к форме:он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", handleFormSubmit);
+/////////////////////////////////////////////////////////////////
+addPopupButton.addEventListener("click", openPopup2);
+closePopup2Button.addEventListener("click", closePopup2);
+form2Element.addEventListener("submit", addFormSubmit);
